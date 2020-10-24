d42aa29983f2c199cca8002df14c90a71e224b5d
mca1scp <Alan.Mitic@uk.bosch.com>
2020-10-24 20:36:10 +0100

    Add console stdin processing

--- build/index.js
+++ build/index.js
@@ -1,43 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var ExprEval_1 = require("./ExprEval");
-/*import * as readline from "readline";
-
-console.log("Console Calculator");
-
-
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let isExitCommand: boolean = false;
-
-const start = async () => {
-    for await (const line of rl) {
-        console.log(line)
-    }
-};
-
-start()
-rl.close();
-*/
-/*//while(!isExitCommand) {
-    rl.question("concalc >> ", (answer) => {
-        switch(answer.toLowerCase()) {
-        case 'exit':
-            isExitCommand = true;
-            break;
-        default:
-            rl.write("Evaluating: " + answer);
-            break;
-        }
-
-        rl.close();
-    });
-//}*/
+//import { ReadLine } from "readline"
+var readline = require('readline');
 console.log("Console Calculator");
 var vs = new Map();
 var ee = new ExprEval_1.ExprEval(vs);
-console.log(ee.evaluate("85|170"));
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+rl.on('line', function (line) {
+    var eeResult = ee.evaluate(line);
+    console.log(eeResult);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,20 @@
 import { ExprEval, VariableStore } from "./ExprEval"
+//import { ReadLine } from "readline"
 
-/*import * as readline from "readline";
-
-console.log("Console Calculator");
-
-
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let isExitCommand: boolean = false;
-
-const start = async () => {
-    for await (const line of rl) {
-        console.log(line)
-    }
-};
-
-start()
-rl.close();
-*/
-/*//while(!isExitCommand) {
-    rl.question("concalc >> ", (answer) => {
-        switch(answer.toLowerCase()) {
-        case 'exit':
-            isExitCommand = true;
-            break;
-        default:
-            rl.write("Evaluating: " + answer);
-            break;
-        }
-
-        rl.close();
-    });
-//}*/
+var readline = require('readline');
 
 console.log("Console Calculator");
 
 let vs: VariableStore = new Map();
 let ee = new ExprEval(vs);
-console.log(ee.evaluate("85|170"))
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line:string){
+    let eeResult = ee.evaluate(line)
+    console.log(eeResult);
+})
